cba611c2ff5f1e8851a40318551ca4bb2755ef75
Jiangjiang <12463gguser@gmail.com>
2025-12-03 18:58:08 +0800

    feat(user-rpc): 初始化userRpc模板内容

---
+++ backend/service/user/user-rpc/userrpc.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"go-zero-learning/service/user/user-rpc/internal/config"
+	"go-zero-learning/service/user/user-rpc/internal/server"
+	"go-zero-learning/service/user/user-rpc/internal/svc"
+	"go-zero-learning/service/user/user-rpc/userrpc"
+
+	"github.com/zeromicro/go-zero/core/conf"
+	"github.com/zeromicro/go-zero/core/service"
+	"github.com/zeromicro/go-zero/zrpc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+var configFile = flag.String("f", "etc/userrpc.yaml", "the config file")
+
+func main() {
+	flag.Parse()
+
+	var c config.Config
+	conf.MustLoad(*configFile, &c)
+	ctx := svc.NewServiceContext(c)
+
+	s := zrpc.MustNewServer(c.RpcServerConf, func(grpcServer *grpc.Server) {
+		userrpc.RegisterUserRpcServer(grpcServer, server.NewUserRpcServer(ctx))
+
+		if c.Mode == service.DevMode || c.Mode == service.TestMode {
+			reflection.Register(grpcServer)
+		}
+	})
+	defer s.Stop()
+
+	fmt.Printf("Starting rpc server at %s...\n", c.ListenOn)
+	s.Start()
+}
