f3ca4544084832f2b5e3baf8f627f7af8070fb49
Jiangjiang <12463gguser@gmail.com>
2025-12-03 20:45:45 +0800

    feat(user-rpc): 添加数据库连接配置和服务上下文更新
    - 在 userrpc.yaml 中添加 DataSource 字段以配置 MySQL 数据库连接
    - 更新 config.go，增加 DataSource 字段以支持数据库连接配置
    - 在 servicecontext.go 中初始化数据库连接并将其添加到服务上下文

--- backend/service/user/user-rpc/internal/config/config.go
+++ backend/service/user/user-rpc/internal/config/config.go
@@ -4,4 +4,7 @@ import "github.com/zeromicro/go-zero/zrpc"
 
 type Config struct {
 	zrpc.RpcServerConf
+
+	// 先只加一个 DataSource, 用来连同一个 MySQL
+	DataSource string `json:"dataSource"`
 }
--- backend/service/user/user-rpc/internal/svc/servicecontext.go
+++ backend/service/user/user-rpc/internal/svc/servicecontext.go
@@ -1,13 +1,25 @@
 package svc
 
-import "go-zero-learning/service/user/user-rpc/internal/config"
+import (
+	"go-zero-learning/common/db"
+	"go-zero-learning/service/user/user-rpc/internal/config"
+
+	"gorm.io/gorm"
+)
 
 type ServiceContext struct {
 	Config config.Config
+	DB     *gorm.DB
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
+	// 暂时和 API 共用同一个 db.InitDB
+	if err := db.InitDB(c.DataSource); err != nil {
+		panic(err)
+	}
+
 	return &ServiceContext{
 		Config: c,
+		DB:     db.GetDB(),
 	}
 }
